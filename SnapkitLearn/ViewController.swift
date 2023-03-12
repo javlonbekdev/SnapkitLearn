a749661ba00a257a6b3446fe5d7541ce6db5d702
javlobekdev <javlonbekdev@gmail.com>
2023-03-12 09:38:56 +0500

    Initial Commit

---
+++ SnapkitLearn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapkitLearn
+//
+//  Created by Javlonbek on 12/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
